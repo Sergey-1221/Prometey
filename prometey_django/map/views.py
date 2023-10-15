b8c2d2b3420bd5f77c8f6e8157fff41325ac49d7
Sergey <59055584+Sergey-1221@users.noreply.github.com>
2023-10-15 12:36:23 +0300

    Update

--- prometey_django/map/views.py
+++ prometey_django/map/views.py
@@ -17,13 +17,14 @@ def getjson_arrow(requests):
     f = open('arrow_1.geojson')
     data = json.load(f)
     data = data["features"]
+    """
     #data_arrow["features"].append(data[0])
     data_arrow["features"].append(data[1])
     #data_arrow["features"].append(data[2])
     data_arrow["features"].append(data[3])
     #data_arrow["features"].append(data[4])
     #data_arrow["features"].append(data[5])
-    data_arrow["features"].append(data[6])
+    #data_arrow["features"].append(data[6])
     #data_arrow["features"].append(data[7])
     data_arrow["features"].append(data[8])
     #data_arrow["features"].append(data[9])
@@ -31,6 +32,7 @@ def getjson_arrow(requests):
     #data_arrow["features"].append(data[11])
     #data_arrow["features"].append(data[12])
     #data_arrow["features"].append(data[13])
+    """
     return JsonResponse(data_arrow)
 
 
